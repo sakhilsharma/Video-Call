2c7565e8c72fe0f2ed89380a973c7c8b73e858e4
Sakhil Sharma <sakhilsharma123@gmail.com>
2026-08-15 15:41:03 +0530

    Fix production socket server URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 const IS_PROD = import.meta.env.PROD;;
 const server = IS_PROD ?
-    "" //production url--> backend on render
+    "https://video-call-meetingbackend.onrender.com" //production url--> backend on render
     :
     "http://localhost:8000"//development url
 
